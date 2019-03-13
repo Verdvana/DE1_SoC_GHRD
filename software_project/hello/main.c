e8854cfc2dbfcb7f30edc441f1b6d894da01c825
Verdvana <739197909@qq.com>
2019-03-13 10:00:17 +0800

    首次更新

---
+++ software_project/hello/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: 2019Äê3ÔÂ5ÈÕ
+ *      Author: Verdvana
+ */
+
+
+#include <stdio.h>
+int main (int argc,char *argv[])
+{
+	printf("hello world\n");
+	return 0;
+}
+
